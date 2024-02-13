4b303053969bc73ccf85ea105140bc4e3e1cf380
AskaXIX <aslan.aslan.14@bk.ru>
2024-02-13 19:35:44 +0600

    code updated

--- bot.py
+++ bot.py
@@ -1,5 +1,23 @@
+# import telebot
+# import requests
+
+# bot = telebot.TeleBot(token="5799740294:AAGEaQWKyGOUKUd4m0f9nnjuyp5iN2Nemd4")
+
+# @bot.message_handler(commands=['start'])
+# def start_message(message):
+#     bot.send_message(message.chat.id, 'Привет! Я бот для поиска информации в Google. Напиши мне свой запрос и я помогу тебе с поиском!')
+
+# @bot.message_handler(content_types=['text'])
+# def send_text(message):
+#     query = message.text
+#     r = requests.get('https://www.google.com/search?q='+query)
+#     bot.send_message(message.chat.id, r.url)
+
+# bot.polling()
+
+
 import telebot
-import requests
+from googlesearch import search
 
 bot = telebot.TeleBot(token="5799740294:AAGEaQWKyGOUKUd4m0f9nnjuyp5iN2Nemd4")
 
@@ -10,7 +28,15 @@ def start_message(message):
 @bot.message_handler(content_types=['text'])
 def send_text(message):
     query = message.text
-    r = requests.get('https://www.google.com/search?q='+query)
-    bot.send_message(message.chat.id, r.url)
+    try:
+        search_results = list(search(query, num=1, stop=1, pause=2.0))
+        if search_results:
+            bot.send_message(message.chat.id, "Вот результаты поиска:")
+            for result in search_results:
+                bot.send_message(message.chat.id, result)
+        else:
+            bot.send_message(message.chat.id, "По вашему запросу ничего не найдено.")
+    except Exception as e:
+        bot.send_message(message.chat.id, f"Произошла ошибка: {e}")
 
 bot.polling()
